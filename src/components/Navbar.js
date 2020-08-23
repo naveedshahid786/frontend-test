1c944d1695a586a9746db49160fe2e700502351a
Naveed Shahid <42672907+naveedshahid786@users.noreply.github.com>
2020-08-23 22:24:24 +0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,6 @@ const Navbar = (props) => {
   return (
     <nav className="navbar navbar-light bg-dark text-white">
       <h3 className="ml-5">Dynamic Webpage</h3>
-        <button className="btn btn-outline-success mr-5 my-2 my-sm-0" >Login</button>
     </nav>
   );
 };
