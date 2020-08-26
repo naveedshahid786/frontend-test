c5388d5faae50751ee3c5b4eaf9d64698df54d5f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-26 06:42:24 +0000

    Deploy naveedshahid786/frontend-test to github.com/naveedshahid786/frontend-test.git:gh-pages

---
+++ precache-manifest.851177fc440d8aaee18a351e8e3b15b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36f74287cfeb76b94c1abc472e319e7f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fe6f1b7ba79a20c1f4cf",
+    "url": "./static/css/main.81b1e578.chunk.css"
+  },
+  {
+    "revision": "a41d7ed2ea6125a450f5",
+    "url": "./static/js/2.a24be4bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a24be4bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe6f1b7ba79a20c1f4cf",
+    "url": "./static/js/main.79c59999.chunk.js"
+  },
+  {
+    "revision": "dbab036f87ca1350243a",
+    "url": "./static/js/runtime-main.851de3a6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
